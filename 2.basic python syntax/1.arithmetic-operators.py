27a394367acecee9169f9e29e57aa5b40d588427
Hasan Ali <hassan.ali@stargallery.ae>
2026-02-10 10:51:29 +0400

    chore: basic python syntax

---
+++ 2.basic python syntax/1.arithmetic-operators.py
@@ -0,0 +1,29 @@
+# 1,2,3,4 etc are these operands when we do arithemetic
+# +,- , == etc are operators
+
+15 + 23
+26 - 50
+20/4
+22/4
+
+y = 1
+
+# double equality
+#  y == 1 true
+
+# by using backslash will execute next line as well
+15 + 31 \
+- 26
+
+
+# indexing string
+"Friday"[4] # op:- d
+
+"Bingo"[0] # op:- B
+
+
+def ten(x):
+    x = 10
+    return x
+
+print(ten(3))
