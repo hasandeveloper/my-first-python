e5c81647d5e8a92a060271c9ca1503a67b75dd2d
Hasan Ali <hassan.ali@stargallery.ae>
2026-02-11 11:38:33 +0400

    chore: operator and conditional

---
+++ 3.operators/2.logical.py
@@ -0,0 +1,13 @@
+True AND True
+True AND False
+False AND False
+False or False
+True or True
+True or False
+False or True
+not True
+not False
+3 > 5 and 4 > 10
+True and not True
+False or not True and True
+True and not True or True
---
+++ 4.conditional/1.conditional.py
@@ -0,0 +1,18 @@
+if 5 == 15/3:
+    print("Hello!")
+
+if 4 > 10:
+    print("Correct")
+else:
+    print("In correct")
+
+
+x = 2
+if x > 200:
+    print("Big")
+elif x > 100 and x <= 200:
+    print("Average")
+elif x > 0 and x <= 100:
+    print("Small")
+elif x < 0:
+    print("Negative")
